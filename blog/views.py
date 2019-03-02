0223522874bb72df4ec3fc432dcd0372d9ff9400
arascch <arash.ask.94@gmail.com>
2019-03-02 21:32:17 +0330

    commit

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post 
+from .forms import EmailPostForm
 
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    paginator = Paginator(object_list, 1) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -38,3 +39,23 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+def post_share(request , post_id):
+    post = get_object_or_404(Post , id = post_id , status = 'published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                                            post.get_absolute_url())
+            subject = '{} ({}) recommends you reading" {}" '.format(cd['Name'] , cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title , post_url , cd['Name'] , cd ['comments'])
+            send_email(subject , message , 'admin@arasch.ir' , [cd['to']])
+            sent = True
+    else :
+        form = EmailPostForm()
+    
+    return render(request  , 'blog/post/share.html' , {'post' : post , 
+                                                       'form' : form , 
+                                                       'sent' : sent})
