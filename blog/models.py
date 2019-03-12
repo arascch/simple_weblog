42d1a99486ad6998fc5a1a3e4daba10d89a9ce71
arasch <arash.ask.94@gmail.com>
2019-03-12 11:18:29 +0330

    commit

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User 
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager): 
     def get_queryset(self): 
@@ -30,7 +30,7 @@ class Post(models.Model):
     
     objects = models.Manager() # The default manager. 
     published = PublishedManager() # Our custom manager.
-
+    tags = TaggableManager()
     class Meta: 
         ordering = ('-publish',) 
 
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,16 @@ from django.views.generic import ListView
 from .models import Post , Comment
 from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-
-def post_list(request):
+def post_list(request , tag_slug =  None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag , slug = tag_slug)
+        object_list = object_list.filter(tags__in = [tag])
     paginator = Paginator(object_list, 1) # 3 posts in each page
     page = request.GET.get('page')
     try:
