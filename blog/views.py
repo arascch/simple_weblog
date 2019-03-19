d44d63d24aa4c7cf160bd3b070f386235bb2feda
arasch <arash.ask.94@gmail.com>
2019-03-19 23:03:43 +0330

    commit

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post , Comment
 from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request , tag_slug =  None):
@@ -27,7 +28,8 @@ def post_list(request , tag_slug =  None):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag' : tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -53,12 +55,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    
+    post_tags_ids = post.tags.values_list('id' , flat = True)
+    similar_posts = Post.published.filter(tags__in = post_tags_ids)\
+                                  .exclude(id = post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags'))\
+                                 .order_by('-same_tags' , '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post ,
                    'comments' : comments,
                    'new_comment':new_comment,
-                   'comment_form':comment_form })
+                   'comment_form':comment_form,
+                   'similar_posts' : similar_posts})
 
 
 class PostListView(ListView):
