506ed26bebdf4204007bcb1262eb192b3fc5f051
arasch <arash.ask.94@gmail.com>
2019-04-10 11:56:34 +0430

    commit

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import EmailPostForm , CommentForm , SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector , SearchQuery , SearchRank , TrigramSimilarity
 
 
 def post_list(request , tag_slug =  None):
@@ -100,14 +100,18 @@ def post_share(request , post_id):
 def post_search(request):
     form = SearchForm()
     query = None 
-    result = []
+    results = []
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title' , weight = 'A') + SearchVector('body' , weight = 'B')
+            search_query  = SearchQuery(query)
             results = Post.objects.annotate(
-                search = SearchVector('title' , 'body'),
-            ).filter(search = query)
+                similarity = TrigramSimilarity('title' , query),
+                search = search_vector,
+                rank = SearchRank(search_vector , search_query)
+            ).filter(similarity__gt = 0.3).order_by('-similarity')
     return render(request , 
                   'blog/post/search.html',
                   {'form' : form ,
